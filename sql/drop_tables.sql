1c208693a757ec157414760bab7de8080e4c4e86
circulardark <circulardark@gmail.com>
2015-09-27 17:22:11 -0700

    initial draft of creating database schema

---
+++ sql/drop_tables.sql
@@ -0,0 +1,15 @@
+/* Relationship tables. */
+DROP TABLE FriendTag;
+DROP TABLE FriendShip;
+
+/* Project tables. */
+DROP TABLE ProjectPartner;
+DROP TABLE ProjectInfo;
+
+/* Role & Skill tables. */
+DROP TABLE Skill;
+DROP TABLE RoleSecondCategory;
+DROP TABLE RoleFirstCategory;
+
+/* The root user table. */
+DROP TABLE UserInfo;
